9c8363a0ea80a14a67ea38e1d3c2402917987fd7
Darlysson Teles <123102867+TelesNascimento25@users.noreply.github.com>
2023-04-30 21:09:59 -0300

    Adição profile

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/TelesNascimento25/Portifolio/main/data/profile.json';
+    const url = "https://raw.githubusercontent.com/TelesNascimento25/Portifolio/main/data/profile.json";
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
